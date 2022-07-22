a0a7ba5594db99ef2ec09f6e3b8967c460b5f34d
kaurpret <102310628+kaurpret@users.noreply.github.com>
2022-07-22 23:23:52 +0530

    Add files via upload

---
+++ spy.py
@@ -0,0 +1,16 @@
+def findSpy(number_of_people,has_information):
+   if number_of_people>0 and len(has_information)<=number_of_people:
+    for i in range(len(has_information)):
+        for j in range(i):
+            x=has_information[i][j]
+            if x==number_of_people:
+                return x
+            else:
+                return 0
+    else:
+        return 1
+if __name__=='__main__':
+    number_of_people=3
+    has_information=[[1,2],[2,3]]
+    print(findSpy(number_of_people,has_information))
+
